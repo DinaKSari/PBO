94850ba27e547c80332ee5e7dfe7cda46ffd98cb
dinaKSari <dinakumala7070@gmail.com>
2025-09-06 21:58:47 +0700

    tugas no 3

---
+++ minggu2/praktikum/lingkaran/TestLingkaran.java
@@ -0,0 +1,13 @@
+package praktikum.lingkaran;
+
+public class TestLingkaran {
+    public static void main(String[] args) {
+        Lingkaran l1 = new Lingkaran();
+        Lingkaran l2 = new Lingkaran();
+
+        l1.phi = 3.14;
+        l1.r = 11;
+        System.out.println("Luas lingkaran: " + l1.hitungLuas());
+        System.out.println("Keliling lingkaran " + l1.hitungKeliling());
+    }
+}
