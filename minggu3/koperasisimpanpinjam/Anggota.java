ea0f4289353bf1e1dc2195adbbf0929ed87e1d88
dinaKSari <dinakumala7070@gmail.com>
2025-09-13 19:18:22 +0700

    membuat program untuk pertanyaan nomor 4

---
+++ minggu3/koperasisimpanpinjam/Anggota.java
@@ -0,0 +1,33 @@
+package koperasisimpanpinjam;
+
+public class Anggota {
+    private int nomorKtp;
+    private String nama;
+    private int limit;
+    private int pinjaman;
+    public Anggota(int nomorKtp, String nama, int limit) {
+        this.nomorKtp = nomorKtp;
+        this.nama = nama;
+        this.limit = limit;
+        this.pinjaman = 0;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public int getLimitPinjaman(){
+        return limit;
+    }
+    public int getJumlahPinjaman(){
+        return pinjaman;
+    }
+    public void pinjam(int newPinjaman){
+        if(newPinjaman < limit){
+            pinjaman =+ newPinjaman;
+        } else{
+            System.out.println("Maaf jumlah pinjaman melebihi limit.");
+        }
+    } 
+    public void angsur(int newAngsur){
+        pinjaman -= newAngsur;
+    }
+}
