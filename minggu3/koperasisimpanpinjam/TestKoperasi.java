c52ef7d67592ce39bffc0071bd9d3e6a88587ab2
dinaKSari <dinakumala7070@gmail.com>
2025-09-13 20:33:32 +0700

    memperbaiki kode

--- minggu3/koperasisimpanpinjam/TestKoperasi.java
+++ minggu3/koperasisimpanpinjam/TestKoperasi.java
@@ -1,9 +1,21 @@
 package koperasisimpanpinjam;
+import java.util.Scanner;
 public class TestKoperasi {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Anggota donny = new Anggota(111333444, "Donny", 5000000);
         System.out.println("Nama Anggota: "+ donny.getNama());
         System.out.println("Limit Pinjaman: "+ donny.getLimitPinjaman());
+        System.out.println("Masukan jumlah uang yang ingin dipinjam: ");
+        int pinjaman = sc.nextInt();
+        donny.pinjam(pinjaman);
+        System.out.println("Jumlah pinjaman saat ini: "+ donny.getJumlahPinjaman());
+        System.out.println("Masukan jumlah angsuran yang ingin dibayar: ");
+        int angsuran = sc.nextInt();
+        System.out.println("\nMembayar angsuran " + angsuran);
+        donny.angsur(angsuran);
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman ());
+        /* 
         System.out.println("\nMeminjam uang 10.000.000...");
         donny.pinjam (10000000);
         System.out.println("Jumlah pinjaman saat ini: "+ donny.getJumlahPinjaman());
@@ -17,6 +29,6 @@ public class TestKoperasi {
         System.out.println("Jumlah pinjaman saat ini: "+ donny.getJumlahPinjaman());
         System.out.println("\nMembayar angsuran 3.000.000");
         donny.angsur (3000000);
-        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman ());
+        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman ());*/
     }
 }
