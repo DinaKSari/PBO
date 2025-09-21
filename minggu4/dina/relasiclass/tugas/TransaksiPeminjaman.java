09100a29857df7bae823dc176900bef6d0a8048c
dinaKSari <dinakumala7070@gmail.com>
2025-09-21 20:40:43 +0700

    membuat class TransaksiPeminjaman, menambahkan atribut di class tersebut dan konstruktor, dan method getIdTransaksi

---
+++ minggu4/dina/relasiclass/tugas/TransaksiPeminjaman.java
@@ -0,0 +1,22 @@
+package minggu4.dina.relasiclass.tugas;
+import java.time.LocalDate;
+public class TransaksiPeminjaman {
+    private String idTransaksi;
+    private LocalDate tanggalPinjam;
+    private LocalDate tanggalKembaliDiharapkan;
+    private LocalDate tanggalKembaliAsli;
+    private Anggota anggota;
+    private Buku buku;
+
+    public TransaksiPeminjaman(String idTransaksi, LocalDate tanggalPinjam, LocalDate tanggalKembaliDiharapkan, Anggota anggota, Buku buku) {
+        this.idTransaksi = idTransaksi;
+        this.tanggalPinjam = tanggalPinjam;
+        this.tanggalKembaliDiharapkan = tanggalKembaliDiharapkan;
+        this.anggota = anggota;
+        this.buku = buku;
+    }
+    public String getIdTransaksi(){
+        return idTransaksi;
+    }
+    public 
+}
