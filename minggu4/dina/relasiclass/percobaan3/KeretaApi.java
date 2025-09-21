851b725fcccf5d0682c8cca694d86bc93f2c4513
dinaKSari <dinakumala7070@gmail.com>
2025-09-21 13:36:21 +0700

    merampungkan kode pada class pegawai, menambahkan class KeretaApi dan merampungkan kode di dalamnya.

---
+++ minggu4/dina/relasiclass/percobaan3/KeretaApi.java
@@ -0,0 +1,57 @@
+package minggu4.dina.relasiclass.percobaan3;
+public class KeretaApi {
+    private String nama;
+    private String kelas;
+    private Pegawai masinis;
+    private Pegawai asisten;
+    // Constructor 3 parameter (tanpa asisten)
+    public KeretaApi(String nama, String kelas, Pegawai masinis) {
+        this.nama = nama;
+        this.kelas = kelas;
+        this.masinis = masinis;
+    }
+    // Constructor 4 parameter (dengan asisten)
+    public KeretaApi(String nama, String kelas, Pegawai masinis, Pegawai asisten) {
+        this.nama = nama;
+        this.kelas = kelas;
+        this.masinis = masinis;
+        this.asisten = asisten;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+    public String getKelas() {
+        return kelas;
+    }
+    public void setMasinis(Pegawai masinis) {
+        this.masinis = masinis;
+    }
+    public Pegawai getMasinis() {
+        return masinis;
+    }
+    public void setAsisten(Pegawai asisten) {
+        this.asisten = asisten;
+    }
+    public Pegawai getAsisten() {
+        return asisten;
+    }
+    public String info() {
+        String info = "";
+        info += "Nama: " + this.nama + "\n";
+        info += "Kelas: " + this.kelas + "\n";
+        info += "Masinis: " + this.masinis.info() + "\n";
+        // Pastikan asisten tidak null sebelum memanggil info()
+        if (this.asisten != null) {
+            info += "Asisten: " + this.asisten.info() + "\n";
+        } else {
+            info += "Asisten: (Tidak ada)\n"; // Teks jika tidak ada asisten
+        }
+        return info;
+    }
+}
--- minggu4/dina/relasiclass/percobaan3/Pegawai.java
+++ minggu4/dina/relasiclass/percobaan3/Pegawai.java
@@ -1,4 +1,27 @@
 package minggu4.dina.relasiclass.percobaan3;
 public class Pegawai {
-    
+    private String nip;
+    private String nama;
+    public Pegawai(String nip, String nama) {
+        this.nip = nip;
+        this.nama = nama;
+    }
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+    public String getNip() {
+        return nip;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getNama() {
+        return nama;
+    }
+    public String info() {
+        String info = "";
+        info += "Nip: " + this.nip + "\n";
+        info += "Nama: " + this.nama + "\n";
+        return info;
+    }
 }
