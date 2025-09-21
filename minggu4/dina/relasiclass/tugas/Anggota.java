f2fe8026cbc1895926c2198c7e0531939783a343
dinaKSari <dinakumala7070@gmail.com>
2025-09-21 20:12:18 +0700

    atribut Anggota dan konstruktor

---
+++ minggu4/dina/relasiclass/tugas/Anggota.java
@@ -0,0 +1,13 @@
+package minggu4.dina.relasiclass.tugas;
+public class Anggota {
+    private String idAnggota;
+    private String nama;
+    private TransaksiPeminjaman[] daftarPeminjaman;
+
+    public Anggota(String idAnggota, String nama) {
+        this.idAnggota = idAnggota;
+        this.nama = nama;
+    }
+    
+    
+}
