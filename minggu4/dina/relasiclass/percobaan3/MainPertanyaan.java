b30c08e69342e32344462bc43951eb11a0749f15
dinaKSari <dinakumala7070@gmail.com>
2025-09-21 15:27:04 +0700

    memperbaiki kode

--- minggu4/dina/relasiclass/percobaan3/MainPertanyaan.java
+++ minggu4/dina/relasiclass/percobaan3/MainPertanyaan.java
@@ -2,6 +2,8 @@ package minggu4.dina.relasiclass.percobaan3;
 public class MainPertanyaan {
     public static void main(String[] args) {
         Pegawai masinis = new Pegawai("1234", "Spongebob Squarepants");
-        Pegawai asisten = new Pegawai("4567", "Patrick Star");
+        KeretaApi keretaApi = new KeretaApi("Gaya Baru", "Bisnis", masinis);
+        System.out.println(keretaApi.info());
+
     }
 }
