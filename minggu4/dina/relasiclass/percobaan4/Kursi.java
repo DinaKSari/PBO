cd5d49a31d4c68726456337e959cf8cb5cd308f3
dinaKSari <dinakumala7070@gmail.com>
2025-09-21 16:01:22 +0700

    merampungkan kode class kursi

--- minggu4/dina/relasiclass/percobaan4/Kursi.java
+++ minggu4/dina/relasiclass/percobaan4/Kursi.java
@@ -1,5 +1,28 @@
 package minggu4.dina.relasiclass.percobaan4;
-
 public class Kursi {
-
+    private String nomor;
+    private Penumpang penumpang;
+    public Kursi(String nomor) {
+        this.nomor = nomor;
+    }
+    public void setNomor(String nomor) {
+        this.nomor = nomor;
+    }
+    public String getNomor() {
+        return nomor;
+    }
+    public void setPenumpang(Penumpang penumpang) {
+        this.penumpang = penumpang;
+    }
+    public Penumpang getPenumpang() {
+        return penumpang;
+    }
+    public String info() {
+        String info = "";
+        info += "Nomor: " + nomor + "\n";
+        if (this.penumpang != null) {
+            info += "Penumpang: " + penumpang.info() + "\n";
+        }
+        return info;
+    }
 }
