bab1e26ed6f7146382541aa2bab5b808731fa0dc
dinaKSari <dinakumala7070@gmail.com>
2025-09-27 06:33:41 +0700

    merampungkan kode

--- quiz1/dina/bossMonster.java
+++ quiz1/dina/bossMonster.java
@@ -1,15 +1,15 @@
 package quiz1.dina;
 public class bossMonster extends Character{
-    private int seranganSpecial;
-    public bossMonster(String name, int health, int attackPower) { 
+    private String bossType;
+    public bossMonster(String name, int health, int attackPower, String bossType) { 
         super(name, health, attackPower); 
-        seranganSpecial = attackPower * 2; 
+        this.bossType = bossType;
     } 
 
     @Override 
     public void attack(Character target) { 
-        int damage = (int)(Math.random() * 10) + 5; // random damage 
-        System.out.println(getName() + " (" + type + ") attacks ferociously! Damage: " + 
+        int damage = 2*((int)(Math.random() * 10) + 5); // random damage lalu damage 2 kali lipat
+        System.out.println(getName() + " (" + bossType + ") attacks ferociously! Damage: " + 
         damage); 
         target.takeDamage(damage); 
     } 
