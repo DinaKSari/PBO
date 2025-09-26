1924a4363f609da44cee786bfa12f348dc928cb6
dinaKSari <dinakumala7070@gmail.com>
2025-09-27 06:12:48 +0700

    mengulang quiz

--- quiz/quiz/dina/Player.java
+++ quiz/quiz/dina/Player.java
@@ -2,8 +2,13 @@ package quiz.quiz.dina;
 public class Player extends Character{
     private int level;
 
-    @Override
-    public void attack(){
+    public Player(String name, int health, int attackPower, int level) {
+        super(name, health, attackPower);
+        this.level = level;
+    }
 
+    @Override
+    public void attack(Character target){
+        
     }
 }
