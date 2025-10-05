9e8a8de5d5454c822c4eff73cdb0193f2d102249
dinaKSari <dinakumala7070@gmail.com>
2025-10-05 16:22:38 +0700

    membuat konstruktor Pelanggaran

--- minggu6/sisteminformasikampus/Pelanggaran.java
+++ minggu6/sisteminformasikampus/Pelanggaran.java
@@ -1,5 +1,14 @@
 package minggu6.sisteminformasikampus;
 
 public class Pelanggaran {
+    private AnggotaKampus pelanggar;
+    private TataTertib peraturanDilanggar;
+    private String tanggal;
 
+    public Pelanggaran(AnggotaKampus pelanggar, TataTertib peraturanDilanggar, String tanggal) {
+        this.pelanggar = pelanggar;
+        this.peraturanDilanggar = peraturanDilanggar;
+        this.tanggal = tanggal;
+    }
+    
 }
