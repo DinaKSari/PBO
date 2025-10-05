6524d7b32e6f061080bf3c9381e20821fda78bfd
dinaKSari <dinakumala7070@gmail.com>
2025-10-05 21:22:07 +0700

    membuat package hewanOOP4 dan class Hewan, Kucing dan main

---
+++ minggu6/hewanOOP4/Main.java
@@ -0,0 +1,11 @@
+package minggu6.hewanOOP4;
+public class Main {
+    public static void main(String[] args) {
+        Kucing kucing = new Kucing("Milo", 2, "Oranye");
+        kucing.info();
+        kucing.berjalan();
+
+        // System.out.println(kucing.nama); // Error, 'nama' adalah private
+        System.out.println(kucing.umur); // Valid, 'umur' adalah protected dan bisa diakses oleh subclass
+    }
+}
