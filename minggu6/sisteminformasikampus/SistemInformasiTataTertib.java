1f18eee5eef56046791cff8a55665e5c29155812
dinaKSari <dinakumala7070@gmail.com>
2025-10-05 16:39:39 +0700

    modifikasi karna salah lihat

--- minggu6/sisteminformasikampus/SistemInformasiTataTertib.java
+++ minggu6/sisteminformasikampus/SistemInformasiTataTertib.java
@@ -4,9 +4,14 @@ public class SistemInformasiTataTertib {
     private Pelanggaran daftarPelanggaran[];
     private int jumlahPelanggaran;
 
-    public void cataPelanggaran(Pelanggaran pelanggaran){
+    public SistemInformasiTataTertib(Pelanggaran[] daftarPelanggaran, int jumlahPelanggaran) {
         daftarPelanggaran = new Pelanggaran[100];
         jumlahPelanggaran = 0;
+    }
+
+    
+    public void cataPelanggaran(Pelanggaran pelanggaran){
+        
     }
 
     public void lihatRiwayatPelanggaran(AnggotaKampus anggota){
