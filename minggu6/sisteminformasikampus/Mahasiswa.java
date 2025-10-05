927d267d62297a82e06c310a66867577ce641df9
dinaKSari <dinakumala7070@gmail.com>
2025-10-05 15:59:55 +0700

    membuat konstruktor mahasiswa

--- minggu6/sisteminformasikampus/Mahasiswa.java
+++ minggu6/sisteminformasikampus/Mahasiswa.java
@@ -1,5 +1,12 @@
 package minggu6.sisteminformasikampus;
 
 public class Mahasiswa {
+    private String nim;
+    private String programStudi;
 
+    public Mahasiswa(String nama, String id, String nim, String programStudi) {
+        this.nim = nim;
+        this.programStudi = programStudi;
+    }
+    
 }
