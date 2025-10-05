d5cee756401ad55d724e615da07ed945a759b10b
dinaKSari <dinakumala7070@gmail.com>
2025-10-05 20:46:06 +0700

    memodifikasi class kucing untuk nambah ras sesuai tugas percobaan 3

--- minggu6/hewanOOP2/Kucing.java
+++ minggu6/hewanOOP2/Kucing.java
@@ -1,7 +1,10 @@
 package minggu6.hewanOOP2;
 class Kucing extends Hewan {
-    public Kucing(String nama, int umur) {
+    String ras;
+    public Kucing(String nama, int umur, String ras) {
         super(nama, umur); // Memanggil konstruktor Hewan
+        this.ras = ras;
         System.out.println("Konstruktor Kucing dipanggil");
+        System.out.println("Ras kucing adalah " + this.ras);
     }
 }
