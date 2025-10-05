b129f06b385186ade56b5571b40a35e3a12e46a9
dinaKSari <dinakumala7070@gmail.com>
2025-10-05 16:26:36 +0700

    membuat method yang ada di SistemIndormasiTataTertib

--- minggu6/sisteminformasikampus/SistemInformasiTataTertib.java
+++ minggu6/sisteminformasikampus/SistemInformasiTataTertib.java
@@ -2,4 +2,11 @@ package minggu6.sisteminformasikampus;
 
 public class SistemInformasiTataTertib {
 
+    public void cataPelanggaran(Pelanggaran pelanggaran){
+
+    }
+
+    public void lihatRiwayatPelanggaran(AnggotaKampus anggota){
+        
+    }
 }
