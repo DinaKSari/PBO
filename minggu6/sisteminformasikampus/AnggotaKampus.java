d595644887676efb4189e278549c691d3a4840a4
dinaKSari <dinakumala7070@gmail.com>
2025-10-05 16:02:27 +0700

    membuat konstruktor anggotaKampus

--- minggu6/sisteminformasikampus/AnggotaKampus.java
+++ minggu6/sisteminformasikampus/AnggotaKampus.java
@@ -1,5 +1,12 @@
 package minggu6.sisteminformasikampus;
 
 public class AnggotaKampus {
+    private String nama;
+    private String id;
 
+    public AnggotaKampus(String nama, String id) {
+        this.nama = nama;
+        this.id = id;
+    }
+    
 }
