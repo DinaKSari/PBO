47b639c7d86f32d12ceb58efba756a628736dd8a
dinaKSari <dinakumala7070@gmail.com>
2025-10-05 16:38:48 +0700

    membuat atribut untuk daftar pelanggaran dengan tipe data array serta atribut jumlah pelanggaran untuk menghitung jumlah yang masuk ke dalam array

--- minggu6/sisteminformasikampus/SistemInformasiTataTertib.java
+++ minggu6/sisteminformasikampus/SistemInformasiTataTertib.java
@@ -1,12 +1,15 @@
 package minggu6.sisteminformasikampus;
 
 public class SistemInformasiTataTertib {
+    private Pelanggaran daftarPelanggaran[];
+    private int jumlahPelanggaran;
 
     public void cataPelanggaran(Pelanggaran pelanggaran){
-
+        daftarPelanggaran = new Pelanggaran[100];
+        jumlahPelanggaran = 0;
     }
 
     public void lihatRiwayatPelanggaran(AnggotaKampus anggota){
-        
+
     }
 }
