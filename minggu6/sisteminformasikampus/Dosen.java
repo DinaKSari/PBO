789b26b4c144f2cb6b2c27ebcc6cb09dec00e06c
dinaKSari <dinakumala7070@gmail.com>
2025-10-05 16:04:25 +0700

    membuat konstruktor Dosen

--- minggu6/sisteminformasikampus/Dosen.java
+++ minggu6/sisteminformasikampus/Dosen.java
@@ -1,5 +1,12 @@
 package minggu6.sisteminformasikampus;
 
 public class Dosen {
+    private String nip;
+    private String departemen;
 
+    public Dosen(String nama, String id, String nip, String departemen) {
+        this.nip = nip;
+        this.departemen = departemen;
+    }
+    
 }
