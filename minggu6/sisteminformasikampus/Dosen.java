1d551dc6ecc17a101bca4040863835c9489483b3
dinaKSari <dinakumala7070@gmail.com>
2025-10-05 16:20:45 +0700

    membuat class child extend class induk

--- minggu6/sisteminformasikampus/Dosen.java
+++ minggu6/sisteminformasikampus/Dosen.java
@@ -1,10 +1,11 @@
 package minggu6.sisteminformasikampus;
 
-public class Dosen {
+public class Dosen extends AnggotaKampus{
     private String nip;
     private String departemen;
 
     public Dosen(String nama, String id, String nip, String departemen) {
+        super(nama, id);
         this.nip = nip;
         this.departemen = departemen;
     }
--- minggu6/sisteminformasikampus/Mahasiswa.java
+++ minggu6/sisteminformasikampus/Mahasiswa.java
@@ -1,10 +1,11 @@
 package minggu6.sisteminformasikampus;
 
-public class Mahasiswa {
+public class Mahasiswa extends AnggotaKampus{
     private String nim;
     private String programStudi;
 
     public Mahasiswa(String nama, String id, String nim, String programStudi) {
+        super(nama, id);
         this.nim = nim;
         this.programStudi = programStudi;
     }
