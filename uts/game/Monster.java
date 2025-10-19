02ec2591a88d3501a76f31b2d54f32b00f608614
dinaKSari <dinakumala7070@gmail.com>
2025-10-19 21:09:00 +0700

    membuat base attack power dan dmg random

--- uts/game/Monster.java
+++ uts/game/Monster.java
@@ -1,12 +1,19 @@
 package uts.game;
+import java.util.Random;
+
 public class Monster extends Enemy {
-    public Monster(String name, int hp, int ap, int threatLevel,
-AttackStrategy strategy) {
+    private static final Random rand = new Random();
+    public Monster(String name, int hp, int ap, int threatLevel, AttackStrategy strategy) {
         super(name, hp, ap, threatLevel, strategy);
     }
 
     @Override
     public void attack(Character target) {
-        // random damage berbasis attackPower / strategy
+        int baseAP = getAttackPower();
+        int minDamage = (int) (baseAP * 0.8);
+        int maxDamage = (int) (baseAP * 1.2);
+        int damage = rand.nextInt(maxDamage - minDamage + 1) + minDamage;
+
+        target.takeDamage(damage);
     }
 }
