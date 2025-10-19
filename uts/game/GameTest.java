051402450624520d7786dc455c7fd5b7c159e4d0
dinaKSari <dinakumala7070@gmail.com>
2025-10-19 23:01:32 +0700

    membuat test untuk game nya

--- uts/game/GameTest.java
+++ uts/game/GameTest.java
@@ -1,8 +1,31 @@
 package uts.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameTest {
     public static void main(String[] args) {
-        Player p = new Player("HeroVipkas", 120, 25, 5, newLevelScaledStrategy(2));
-        p.addSkill(new HealSkill(15));
-        // tambah efek awal, musuh, boss, lalu jalankan Battle
+        AttackStrategy playerStrategy = new LevelScaledStrategy(2);
+        AttackStrategy enemyStrategy = new FixedStrategy(); 
+        Player hero = new Player("Dina", 600, 420, 5, playerStrategy);
+        
+        hero.addSkill(new HealSkill(20));
+        hero.addSkill(new PiercingStrike(1.5)); 
+        hero.addEffect(new Regen(5, 3)); 
+
+        List<Character> teamA = new ArrayList<>();
+        teamA.add(hero);
+
+        Monster goblin = new Monster("Goblin", 40, 10, 1, enemyStrategy);
+        
+        BossMonster dragon = new BossMonster("Dragon", 80, 20, 5, enemyStrategy);
+        
+        dragon.addEffect(new Shield(10, 2));
+        
+        List<Character> teamB = new ArrayList<>();
+        teamB.add(goblin);
+        teamB.add(dragon);
+        Battle battle = new Battle(teamA, teamB);
+        battle.run();
     }
 }
