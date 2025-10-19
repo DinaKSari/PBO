d6592c1397a77c0d89a2c1067fb725030a580cae
dinaKSari <dinakumala7070@gmail.com>
2025-10-19 22:42:49 +0700

    menambahkan fitur auto target dan memulai pertarungan

--- uts/game/Battle.java
+++ uts/game/Battle.java
@@ -12,7 +12,106 @@ public class Battle {
         this.teamB = new ArrayList<>(teamB);
     }
 
-    
+    public void run() {
+        System.out.println("=== BATTLE DIMULAI! ===");
+        printTeamStatus("Team A", teamA);
+        printTeamStatus("Team B", teamB);
+        int turn = 1;
+        while (isTeamAlive(teamA) && isTeamAlive(teamB)) {
+            System.out.println("\n--- Turn " + turn + " ---");
+
+            Character attacker = null;
+            Character target = null;
+
+            if (turn % 2 != 0) {
+                attacker = findNextLivingAttacker(teamA, nextAttackerIndexA);
+                if (attacker != null) {
+                    nextAttackerIndexA = (teamA.indexOf(attacker) + 1) % teamA.size();
+                }
+                target = findTargetFor(attacker, teamB);
+                
+            } else {
+                attacker = findNextLivingAttacker(teamB, nextAttackerIndexB);
+                if (attacker != null) {
+                    nextAttackerIndexB = (teamB.indexOf(attacker) + 1) % teamB.size();
+                }
+                target = findTargetFor(attacker, teamA);
+            }
+            if (attacker != null && target != null) {
+                attacker.performTurn(target);
+            } else if (attacker != null && target == null) {
+                System.out.println(attacker.getName() + " melihat tidak ada target tersisa.");
+            }
+            
+            turn++;
+        }
+    }
+
+    private Character findNextLivingAttacker(List<Character> team, int startIndex) {
+        for (int i = startIndex; i < team.size(); i++) {
+            if (team.get(i).isAlive()) {
+                return team.get(i);
+            }
+        }
+        for (int i = 0; i < startIndex; i++) {
+            if (team.get(i).isAlive()) {
+                return team.get(i);
+            }
+        }
+        return null;
+    }
+
+    private Character findTargetFor(Character attacker, List<Character> targetTeam) {
+        if (attacker == null) return null;
+        Character bestTarget = null;
+        if (attacker instanceof Player) {
+            Enemy currentBestTarget = null;
+            for (Character target : targetTeam) {
+                if (!target.isAlive() || !(target instanceof Enemy)) continue;
+                Enemy enemyTarget = (Enemy) target;
+                if (currentBestTarget == null) {
+                    currentBestTarget = enemyTarget;
+                } else {
+                    if (enemyTarget.getThreatLevel() > currentBestTarget.getThreatLevel()) {
+                        currentBestTarget = enemyTarget;
+                    }
+                    else if (enemyTarget.getThreatLevel() == currentBestTarget.getThreatLevel()) {
+                        if (enemyTarget.getHealth() < currentBestTarget.getHealth()) {
+                            currentBestTarget = enemyTarget;
+                        }
+                    }
+                }
+            }
+            bestTarget = currentBestTarget;
+        } 
+        else if (attacker instanceof Enemy) {
+            Player currentBestTarget = null;
+            
+            for (Character target : targetTeam) {
+                if (!target.isAlive() || !(target instanceof Player)) continue;
+                Player playerTarget = (Player) target;
+                if (currentBestTarget == null) {
+                    currentBestTarget = playerTarget;
+                } else {
+                    if (playerTarget.getHealth() > currentBestTarget.getHealth()) {
+                        currentBestTarget = playerTarget;
+                    }
+                }
+            }
+            bestTarget = currentBestTarget;
+        }
+        if (bestTarget == null) {
+            for (Character target : targetTeam) {
+                if (target.isAlive()) {
+                    bestTarget = target;
+                    break;
+                }
+            }
+        }
+
+        return bestTarget;
+    }
+
     private boolean isTeamAlive(List<Character> team) {
         for (Character member : team) {
             if (member.isAlive()) {
