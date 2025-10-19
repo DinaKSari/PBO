50b13b5c7be6b4c595c7647fd0be642143685468
dinaKSari <dinakumala7070@gmail.com>
2025-10-19 22:34:48 +0700

    menyimpan giliran siapa per team

--- uts/game/Battle.java
+++ uts/game/Battle.java
@@ -2,9 +2,11 @@ package uts.game;
 import java.util.*;
 
 public class Battle {
-private final List<Character> teamA;
+    private final List<Character> teamA;
     private final List<Character> teamB;
 
+    private int nextAttackerIndexA = 0;
+    private int nextAttackerIndexB = 0;
     public Battle(List<Character> teamA, List<Character> teamB) {
         this.teamA = teamA; this.teamB = teamB;
     }
