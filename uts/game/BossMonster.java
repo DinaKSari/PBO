6b4adb6c304e7ae7116f4a4893848e78ef6ecfd7
dinaKSari <dinakumala7070@gmail.com>
2025-10-19 21:12:08 +0700

    naikan counter internal

--- uts/game/BossMonster.java
+++ uts/game/BossMonster.java
@@ -3,13 +3,12 @@ package uts.game;
 public class BossMonster extends Enemy {
     private int turnCounter = 0;
 
-    public BossMonster(String name, int hp, int ap, int threatlevel,
-AttackStrategy strategy) {
+    public BossMonster(String name, int hp, int ap, int threatlevel, AttackStrategy strategy) {
         super(name, hp, ap, threatlevel, strategy);
     }
 
     @Override
     public void attack(Character target) {
-        // Rage Strike: 2x damage jika hp < 50% atau setiap 3 giliran
+        turnCounter++;
     }
 }
