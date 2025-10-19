4117605c302ca7bbe483f8b3cfa950d1ae79b366
dinaKSari <dinakumala7070@gmail.com>
2025-10-19 21:57:08 +0700

    menambahkan class Shield untuk implementasi interface StatusEffect

---
+++ uts/game/Shield.java
@@ -0,0 +1,12 @@
+package uts.game;
+
+public class Shield implements StatusEffect {
+    private int flatReduce;
+    private int duration;
+
+    public Shield(int flatReduce, int duration) {
+        this.flatReduce = Math.max(0, flatReduce);
+        this.duration = Math.max(1, duration);
+    }
+
+}
