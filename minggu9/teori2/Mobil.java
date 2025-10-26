c1ee94d3e719fdd075fce574f0b3287f0b1a6967
dinaKSari <dinakumala7070@gmail.com>
2025-10-26 19:54:40 +0700

    mmbuat exercise kedua. membuat file class Mobile sebagai superclass

---
+++ minggu9/teori2/Mobil.java
@@ -0,0 +1,16 @@
+package minggu9.teori2;
+public class Mobil {
+    private String jenis;
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    public String getJenis() {
+        return this.jenis;
+    }
+
+    public String getJenisBahanBakar() {
+        return "Bahan Bakar Umum";
+    }
+}
