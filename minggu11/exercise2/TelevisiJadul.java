f24e7825df983c84d54ba788f8ff662bab03cadb
dinaKSari <dinakumala7070@gmail.com>
2025-11-09 20:53:43 +0700

    membuat subclass TelevisiJadul

---
+++ minggu11/exercise2/TelevisiJadul.java
@@ -0,0 +1,14 @@
+package minggu11.exercise2;
+
+public class TelevisiJadul extends Elektronik {
+    private String modeInput;
+
+    public TelevisiJadul() {
+        super();
+        this.modeInput = "DVI"; 
+    }
+
+    public String getModeInput() {
+        return modeInput;
+    }
+}
