2fe83b397eb966ea68fdf42a151df16060582607
dinaKSari <dinakumala7070@gmail.com>
2025-11-09 23:08:10 +0700

    membuat class Barrier dan method getter nya

---
+++ minggu11/tugas/Barrier.java
@@ -0,0 +1,18 @@
+package minggu11.tugas;
+
+public class Barrier implements Destroyable {
+    private int strength;
+
+    public Barrier(int strength) {
+        this.strength = strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+}
--- minggu11/tugas/JumpingZombie.java
+++ minggu11/tugas/JumpingZombie.java
@@ -19,13 +19,11 @@ public class JumpingZombie extends Zombie {
 
     @Override
     public void destroyed() {
-        // Logika disesuaikan agar output 100 -> 66 dalam 4 langkah
         this.health = (int) Math.round(this.health * 0.9);
     }
 
     @Override
     public String getZombieInfo() {
-        // Menambahkan header spesifik
         return "Jumping Zombie Data =\n" + super.getZombieInfo();
     }
 }
