6947e32f6081cfa4601cce6bd9512e65ae9ece07
dinaKSari <dinakumala7070@gmail.com>
2025-11-09 20:30:39 +0700

    modifikasi class

--- minggu11/Manajer.java
+++ minggu11/Manajer.java
@@ -3,21 +3,13 @@ package minggu11;
 public class Manajer extends Pegawai {
     private int tunjangan;
 
-    // Konstruktor Manajer
     public Manajer(String nama, int gaji, int tunjangan) {
-        // Memanggil konstruktor superclass (Pegawai)
         super(nama, gaji);
         this.tunjangan = tunjangan;
     }
 
-    // Override method getGaji() dari superclass
-    @Override
-    public int getGaji() {
-        // Gaji total = gaji pokok + tunjangan
-        return super.getGaji() + tunjangan;
-    }
+    // getGaji() tidak di-override
 
-    // Method khusus Manajer
     public int getTunjangan() {
         return tunjangan;
     }
--- minggu11/Pegawai.java
+++ minggu11/Pegawai.java
@@ -1,25 +1,20 @@
 package minggu11;
 public class Pegawai {
-    // Menggunakan 'protected' agar bisa diakses oleh subclass
     protected String nama;
     protected int gaji;
 
-    // Konstruktor default
     public Pegawai() {
     }
 
-    // Konstruktor dengan parameter
     public Pegawai(String nama, int gaji) {
         this.nama = nama;
         this.gaji = gaji;
     }
 
-    // Method untuk mendapatkan gaji pokok
     public int getGaji() {
-        return gaji;
+        return gaji; // Mengembalikan gaji pokok
     }
 
-    // Method tambahan (tidak ada di diagram, tapi logis untuk ada)
     public String getNama() {
         return nama;
     }
