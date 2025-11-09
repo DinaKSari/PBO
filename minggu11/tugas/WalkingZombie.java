1e262e5c2cf2c9e10e9eac96c401060b85ed788a
dinaKSari <dinakumala7070@gmail.com>
2025-11-09 23:03:44 +0700

    membuat method heal yang di override

--- minggu11/tugas/WalkingZombie.java
+++ minggu11/tugas/WalkingZombie.java
@@ -5,4 +5,15 @@ public class WalkingZombie extends Zombie {
     public WalkingZombie(int health, int level) {
         super(health, level);
     }
+    @Override
+    public void heal() {
+        if (level == 1) {
+            health = (int) (health * 1.20);
+        } else if (level == 2) {
+            health = (int) (health * 1.30);
+        } else if (level == 3) {
+            health = (int) (health * 1.40);
+        }
+    }
+
 }
