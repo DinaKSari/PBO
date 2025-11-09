b3fca6b8253568739f308f997202449dec928533
dinaKSari <dinakumala7070@gmail.com>
2025-11-09 20:31:00 +0700

    modifikasi class

--- minggu11/Programmer.java
+++ minggu11/Programmer.java
@@ -3,21 +3,13 @@ package minggu11;
 public class Programmer extends Pegawai {
     private int bonus;
 
-    // Konstruktor Programmer
     public Programmer(String nama, int gaji, int bonus) {
-        // Memanggil konstruktor superclass (Pegawai)
         super(nama, gaji);
         this.bonus = bonus;
     }
 
-    // Override method getGaji() dari superclass
-    @Override
-    public int getGaji() {
-        // Gaji total = gaji pokok + bonus
-        return super.getGaji() + bonus;
-    }
+    // getGaji() tidak di-override
 
-    // Method khusus Programmer
     public int getBonus() {
         return bonus;
     }
